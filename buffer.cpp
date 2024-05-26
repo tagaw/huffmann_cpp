d1538796b70a8996c4a416db060ea37d7ef42840
Tyler Gaw <tagaw@ucsc.edu>
2024-05-26 16:51:09 -0700

    prelim output functionality

--- buffer.cpp
+++ buffer.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "buffer.h"
 
+// @brief sets ith bit in array
 void set_i_bit(uint8_t* bit_array, int i) {
     bit_array[i/8] |= (uint8_t)1 << (i % 8);
     return;
 }
 
+// @brief clears ith bit in array
 void clr_i_bit(uint8_t* bit_array, int i) {
     bit_array[i/8] &= ~((uint8_t)1 << (i % 8));
     return;
@@ -16,6 +18,7 @@ HuffmanBuffer::HuffmanBuffer(std::streambuf* buffer, HuffmanAlgorithm* hfa): buf
 
     in_buffer = new uint8_t[BLOCK];
     out_buffer = new uint8_t[BLOCK];
+    code_bits = new uint8_t[BLOCK];
 
     char* _tempcast = (char*)in_buffer;
     char* _tempcast2 = (char*)out_buffer;
@@ -29,26 +32,56 @@ HuffmanBuffer::HuffmanBuffer(std::streambuf* buffer, HuffmanAlgorithm* hfa): buf
 HuffmanBuffer::~HuffmanBuffer() {
     delete[] in_buffer;
     delete[] out_buffer;
+    delete[] code_bits;
 }
 
-void HuffmanBuffer::compress() {
+void HuffmanBuffer::compress(int bytes_in) {
     Code* table = huffman->get_table();
-    int bytes_in = gptr() - eback();
     for (int i = 0; i < bytes_in; i++) {
         int symb = in_buffer[i];
         Code c = table[symb];
+
         for (int j = 0; j < c.size(); j++) {
             if (bit_idx >= max_bit)  {
-                sputn((char*)out_buffer,BLOCK-1);
+                buffer->sputn((char*)code_bits,BLOCK);
                 bit_idx = 0;
             }
             if (c.get_bit(i)) {
-                set_i_bit(out_buffer,bit_idx);
+                set_i_bit(code_bits,bit_idx);
             }
             else {
-                clr_i_bit(out_buffer,bit_idx);
+                clr_i_bit(code_bits,bit_idx);
             }
             bit_idx += 1;
         }
     }
+}
+
+void HuffmanBuffer::finish_code() {
+    if (bit_idx < max_bit) {
+        for (int i = bit_idx % 8; i > 0; i = (i+1)%8 ) {
+            clr_i_bit(code_bits,bit_idx);
+            bit_idx += 1;
+        }
+    }
+    buffer->sputn((char*)code_bits,(bit_idx/8));
+}
+
+std::streambuf::int_type HuffmanBuffer::overflow(int c = EOF) {
+    int wrote = pptr() - pbase();
+    if (wrote > 0) {
+        this->compress(wrote);
+    }
+    setp((char*)out_buffer,(char*)out_buffer+BLOCK);
+    if (!traits_type::eq_int_type(c,traits_type::eof())) {
+        sputc(c);
+    }
+    return traits_type::not_eof(c);
+}
+
+std::streambuf::int_type HuffmanBuffer::sync() {
+    int res = this->overflow(traits_type::eof());
+    this->finish_code();
+    buffer->pubsync();
+    return traits_type::eq_int_type(res, traits_type::eof()) ? -1 : 0;
 }
--- buffer.h
+++ buffer.h
@@ -9,24 +9,34 @@ private:
 
     uint8_t* in_buffer;
     uint8_t* out_buffer;
+    uint8_t* code_bits;
 
     HuffmanAlgorithm* huffman;
 
     int bit_idx;
     const int max_bit = BLOCK * 8;
 
-    void compress();
+    // @brief performs huffman compression of n bytes
+    void compress(int bytes_in);
+
+    // @brief flushes codes to location
+    void finish_code();
 
 protected:
+    // @brief gets characters in buffer for reading
     virtual std::streambuf::int_type underflow();
 
+    // @brief gets characters in buffer for writing
     virtual std::streambuf::int_type overflow(int c = EOF);
 
+    // @brief syncs location in file with buffer
     virtual std::streambuf::int_type sync();
 
 public:
+    // @brief Wraps an existing buffer and assigns associated huffman algorithm
     HuffmanBuffer(std::streambuf* buffer, HuffmanAlgorithm* hfa);
 
+    // @brief Clears all allocated data
     ~HuffmanBuffer();
 
 };
