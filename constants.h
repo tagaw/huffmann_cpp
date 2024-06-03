0a8137caab9c435d8fd97e28852ec0f3d4f191de
Tyler Gaw <tagaw@ucsc.edu>
2024-06-03 13:27:35 -0700

    support for header structs for decoding purposes

--- constants.h
+++ constants.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <cstdint>
+#include <iostream>
 
 #define BLOCK 4096
 #define ASCII 256
 #define MAX_CODE (ASCII / 8)
+#define MAGIC_NUMBER 0xDEADBEEF
+
+typedef struct {
+    uint32_t magic;
+    uint16_t tree_size;
+    uint64_t file_size;
+    void print_h(void) {
+        std::cerr << magic << "\n" << tree_size << "\n" << file_size << std::endl;
+    } 
+} Header;
